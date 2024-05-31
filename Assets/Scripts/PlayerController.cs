53d80ad548ecf50e384f9c209ae2f84be5f37cad
ngHaiDangDev <nguyenhaidang04.work@gmail.com>
2024-06-01 01:15:14 +0700

    fix current anim sliding

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,8 +21,11 @@ public class PlayerController : MonoBehaviour
     [Header("Wall")]
     [SerializeField] private float wallCheckDistance = 0.4f;
     [SerializeField] private Transform wallCheck;
+    [SerializeField] private float movementForceInAir;
     private bool isWallSliding;
+    //private bool isTouchingWall;
     private float wallSlideSpeed;
+
 
 
     private void Start()
@@ -33,7 +36,8 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        CheckIsWallSliding();
+        CheckIfWallSliding();
+        //CheckSurroundings();
         PlayerMovement();
         PlayerJump();
 
@@ -42,8 +46,21 @@ public class PlayerController : MonoBehaviour
     private void PlayerMovement()
     {
         horizontal = Input.GetAxis("Horizontal");
+        if (isGrounded())
+        {
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
 
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        } else if (!isGrounded() && !isWallSliding && horizontal != 0)
+        {
+            Vector2 forceAdd = new Vector2(movementForceInAir * horizontal, 0);
+            rb.AddForce(forceAdd);
+
+            if (Mathf.Abs(rb.velocity.x) > speed)
+            {
+                rb.velocity = new Vector2(speed * horizontal, rb.velocity.y);
+            }
+        }
+
         anim.SetBool("Walk", horizontal != 0);
 
         //Wall Sliding
@@ -59,6 +76,7 @@ public class PlayerController : MonoBehaviour
         Flip();
     }
 
+
     private void PlayerJump()
     {
         if (Input.GetKey(KeyCode.Space) && isGrounded())
@@ -68,16 +86,14 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void CheckIsWallSliding()
+    private void CheckIfWallSliding()
     {
-        if (isWalled() && !isGrounded() && rb.velocity.y < 0)
-        {
-            Debug.Log("Yes");
+        if (isWalled() && !isGrounded())
+        { 
             isWallSliding = true;
         }
         else
         {
-            Debug.Log("NO");
             isWallSliding = false;
         }
     }
@@ -103,4 +119,8 @@ public class PlayerController : MonoBehaviour
         return Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, groundLayer);
     }
 
+    /*    private void CheckSurroundings()
+        {
+            isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, groundLayer);
+        }*/
 }
